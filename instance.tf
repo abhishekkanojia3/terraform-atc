833250f80b670fb7e386f8bc6bb62c4745d9b557
abhishekkanojia3 <43204637+abhishekkanojia3@users.noreply.github.com>
2021-11-29 21:09:37 +0530

    docker container

--- instance.tf
+++ instance.tf
@@ -1,14 +1,28 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
 resource "aws_instance" "instance" {
-  ami           = "ami-0d8ad3ab25e7abc51" # us-west-2
+  ami           = data.aws_ami.amazon_linux_2.id 
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet2.id
   vpc_security_group_ids = [aws_security_group.SG.id]
-  key_name = "cloud_itm"
+  key_name = "cloud_itm_canada"
   associate_public_ip_address = "true"
-  provisioner "local-exec" {
-
-    command = "echo ${aws_instance.instance.public_ip} >> ${var.files_name}"
-  }
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo docker run -p 80:80 httpd
+  EOF
   tags = {
     Name = "${var.files_name}"
   }
@@ -16,16 +30,21 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_instance" "instance2" {
-  ami           = "ami-0d8ad3ab25e7abc51" # us-west-2
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.SG.id]
-  key_name = "cloud_itm"
+  key_name = "cloud_itm_canada"
   associate_public_ip_address = "true"
-  provisioner "local-exec" {
-
-    command = "echo ${aws_instance.instance2.public_ip} >> ${var.files_name}"
-  }
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo docker run -p 80:80 httpd
+  EOF
   tags = {
     Name = "${var.files_name}"
   }
